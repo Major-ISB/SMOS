331552e233244019fbe11af19ba30e1766a6aded
SIERRA Julien <sierra.julien38@gmail.com>
2026-04-26 20:47:44 +0200

    Update asmlib.asm

--- Source/current/kernel/asmlib.asm
+++ Source/current/kernel/asmlib.asm
@@ -1,16 +1,23 @@
 bits 16
-
+global bprint
 section .text
 
-global cprint
+bprint:
+    push ebp          ; 32-bit for gcc
+    mov  ebp, esp
+    push esi
 
-cprint:
+    mov  esi, [ebp+8] ; 1st argument (str)
+.loop:
     lodsb
-    or al, al
-    jz .cp_done
-    mov ah, 0x0E
-    int 0x10
-    jmp cprint
-
+    test al, al
+    
+    jz   .cp_done
+    mov  ah, 0x0E
+    int  0x10
+    
+    jmp  .loop
 .cp_done:
+    pop  esi
+    pop  ebp
     ret
