2b003b446da8a6035b5abea070f656ed4d00db51
SIERRA Julien <sierra.julien38@gmail.com>
2026-04-26 20:48:48 +0200

    Update kernel_entry.asm

--- Source/current/kernel/kernel_entry.asm
+++ Source/current/kernel/kernel_entry.asm
@@ -4,7 +4,13 @@
 [global _start]
 
 _start:
+    ; After jump CS=0x1000. Updating stack
+    mov ax, cs
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0xFFFF
+
     call kmain
-    jmp $
 
 %include "asmlib.asm"
