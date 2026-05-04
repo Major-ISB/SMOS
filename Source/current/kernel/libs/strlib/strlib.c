7b9a5f07f3087291414d85d744a6aef435b7ca91
SIERRA Julien <sierra.julien38@gmail.com>
2026-05-04 15:41:36 +0200

    Update strlib.c

--- Source/current/kernel/libs/strlib/strlib.c
+++ Source/current/kernel/libs/strlib/strlib.c
@@ -1,4 +1,6 @@
+#include "strlib.h"
 
+// String lenght
 int strlen(const char* str)
 {
     int lenght = 0;
@@ -12,21 +14,23 @@ int strlen(const char* str)
     return lenght;
 }
 
+// String comparison
 void strcmp(const char* a, const char* b)
 {
-    int egals = 0; // 0 = no
+    int egals = 0; // 0 = yes
 
     if (strlen(a) == strlen(b))
+    {
         while(*a && *b)
         {
             if (*a != *b)
-                egals = 1;
+                egals = 1; // not egals
                 break;
         }
     }
     else
     {
-        egals = 1;
+        egals = 1;  // not egals
     }
 
     return egals;
