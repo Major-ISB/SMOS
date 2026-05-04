2b385e6a6929bd7a63eecaa96209f2c2cc454526
SIERRA Julien <sierra.julien38@gmail.com>
2026-05-04 15:39:37 +0200

    Update strlib.c

--- Source/current/kernel/libs/strlib/strlib.c
+++ Source/current/kernel/libs/strlib/strlib.c
@@ -1,5 +1,33 @@
 
+int strlen(const char* str)
+{
+    int lenght = 0;
+
+    while(*str)
+    {
+        lenght++;
+        *str++;
+    }
+
+    return lenght;
+}
+
 void strcmp(const char* a, const char* b)
 {
-    
+    int egals = 0; // 0 = no
+
+    if (strlen(a) == strlen(b))
+        while(*a && *b)
+        {
+            if (*a != *b)
+                egals = 1;
+                break;
+        }
+    }
+    else
+    {
+        egals = 1;
+    }
+
+    return egals;
 }
