a6ec359f3b112bc42d4d87b7929570ec7f76dc0f
Major <260880063+Major-ISB@users.noreply.github.com>
2026-09-07 18:54:36 +0200

    Improve kernel_entry.asm structure and comments
    Refactor kernel entry assembly for clarity and organization.

--- Source/current/kernel/kernel_entry.asm
+++ Source/current/kernel/kernel_entry.asm
@@ -1,25 +1,19 @@
 ;; kernel_entry.asm => kernel.c loader
 
-bits 32
+bits 32		;nasm directive
 section .text
-    align 4
-    dd 0x1BADB002
-    dd 0x00
-    dd - (0x1BADB002 + 0x00)
+	;multiboot spec
+	align 4
+	dd 0x1BADB002			;magic
+	dd 0x00				;flags
+	dd - (0x1BADB002 + 0x00)	;checksum. m+f+c should be zero
 
 global _start
-extern kmain
+extern kmain	;kmain is defined in the kernel.c file
 
 _start:
-    cli
-    mov esp, stack_top
-    call kmain
-    jmp $
+	cli  ; stop interrupts
+	call k_main
+	hlt ; halt the CPU
 
 %include "libs/asmlib.asm"
-
-section .bss
-align 16
-stack_bottom:
-    resb 16384
-stack_top:
