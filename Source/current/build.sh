0eb36f5de27a9b95d3e06081c0e1529ccb164e9a
Major <260880063+Major-ISB@users.noreply.github.com>
2026-09-07 18:49:34 +0200

    Reset build.sh for clearer hand-made version
    Reset build.sh for a clearer future version and adjust the build process.

--- Source/current/build.sh
+++ Source/current/build.sh
@@ -1,6 +1,7 @@
+# !!! Build.sh reset for a future clearer hand-made version
+
 #!/bin/bash
 
-#sudo rm -R ./build
 mkdir -p ./build
 
 RED='\033[0;31m'
@@ -17,31 +18,17 @@ echo ""
 echo ""
 
 printf "${NC}[1]: Compiling source files..."
-nasm -f bin ./boot/boot.asm -o ./build/boot.bin
 
-# Kernel entry
-nasm -f elf32 ./kernel/kernel_entry.asm -I kernel/ -o ./build/kernel_entry.o
-
-# C in 32-bit
-gcc -m32 -ffreestanding -fno-pie -fno-stack-protector -nostdlib -c ./kernel/kernel.c -o ./build/kernel.o
-gcc -m32 -ffreestanding -fno-pie -fno-stack-protector -nostdlib -c ./kernel/libs/strlib/strlib.c -o ./build/strlib.o
-
-# Linker
-ld -m elf_i386 -T link.ld -o ./build/kernel.bin \
-    ./build/kernel_entry.o ./build/kernel.o ./build/strlib.o
-    
 printf " ${RED}Done!\n"
 
 printf "${NC}[2]: Creating floppy image..."
-cat ./build/boot.bin ./build/kernel.bin > ./build/smos-x86.img
-truncate -s 1440k ./build/smos-x86.img
+
 printf " ${RED}Done!\n"
 
 printf "${NC}[3]: Launching qemu..."
 
 if command -v qemu-system-x86_64 &> /dev/null; then
     printf " ${RED}Launched!\n"
-    qemu-system-x86_64 -kernel ./build/kernel.bin
 else
     printf " ${RED}/!\ Cannot find qemu, aborting...!\n"
 fi
